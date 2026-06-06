c05b95bc3408d5048ed3b02ed408d1588aa62d68
xlgummies <xlgummies@gmail.com>
2026-06-06 13:59:53 -0400

    Create supabase.js

---
+++ src/supabase.js
@@ -0,0 +1,6 @@
+import { createClient } from "@supabase/supabase-js";
+
+export const supabase = createClient(
+  "https://cznfpsdnluogfaegtmzz.supabase.co",
+  "sb_publishable_8IhowT8NzJfadv7BJeXhpw_VC2hURRz"
+);
